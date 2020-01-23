cfc97f6dfa2a8675586834de3e9f7ac9254348d6
loharkalpeah <59511378+loharkalpeah@users.noreply.github.com>
2020-01-23 10:47:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,45 @@
 # 45 * 3 = 555 , 56+9=77 , 56/6=4
 # Faulty Calculater
 
-a=int(input("Enter number #1:"))
-b=int(input("Enter number #2:"))
+while True:
+    a=int(input("Enter number #1:"))
+    b=int(input("Enter number #2:"))
 
-query=input("What you do?:")
-print("Enter + for Addition")
-print("Enter - for Subtraction")
-print("Enter * for Multiplication")
-print("Enter / for Divition")
+    query=input("What you do?:")
+    print("Enter + for Addition")
+    print("Enter - for Subtraction")
+    print("Enter * for Multiplication")
+    print("Enter / for Divition")
 
-if a==45:
-    if b==3:
-        if query=="+":
-            print("Your Answer is 555.")
+    if a==45:
+        if b==3:
+            if query=="+":
+                print("Your Answer is 555.")
 
-if a==56:
-    if b==9:
-        if query=="+":
-            print("Your Answer is 77.")
-    elif b==6:
-        if query=="/":
-            print("Your Answer is 4.")
+    if a==56:
+        if b==9:
+            if query=="+":
+                print("Your Answer is 77.")
+        elif b==6:
+            if query=="/":
+                print("Your Answer is 4.")
 
-if a!=45 or b!=3:
-    if a!=56 or b != 9:
-        if b !=6:
-           if query=="+":
-               print("Your Answer is",a+b)
-           elif query=="-":
-               print("Your Answer is",a-b)
-           elif query=="*":
-               print("Your Answer is",a*b)
-           elif query=="/":
-               if b==0:
-                   print("Division is not Possible.")
-               else:
-                   print("Your Answer is",a/b)
+    if a!=45 or b!=3:
+        if a!=56 or b != 9:
+            if b !=6:
+                if query=="+":
+                    print("Your Answer is",a+b)
+                elif query=="-":
+                    print("Your Answer is",a-b)
+                elif query=="*":
+                    print("Your Answer is",a*b)
+                elif query=="/":
+                    if b==0:
+                        print("Division is not Possible.")
+                    else:
+                        print("Your Answer is",a/b)
+   res=int(input("Do You Want To do again press 1(means yes) or 0(means no)"))
+   if res == 0:
+        break
+        exit
+input("Press Enter")
